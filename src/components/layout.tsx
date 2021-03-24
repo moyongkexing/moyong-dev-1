85c85a4e7fa7dd209d4dd3d79c7d7070dcf44ed3
moyongkexing <moyongkexing@gmail.com>
2021-03-24 21:01:15 +0900

    🚚 rename import

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,13 @@
 import type { ReactNode, VFC } from "react";
-import { Footer } from "src/components/Footer";
-import { Header } from "src/components/Header";
+import Footer from "src/components/Footer";
+import Header from "src/components/Header";
 
 export const Layout: VFC<{ children: ReactNode }> = (props) => {
   return (
     <div className="bg-white w-full flex flex-col justify-center items-center shadow-xl border-2 border-gray-300 md:w-11/12 xl:w-10/12">
-      <Header />
+      <Header/>
       <main className="">{props.children}</main>
-      <Footer />
+      <Footer/>
     </div>
   );
 };

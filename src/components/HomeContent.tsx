237f5d2ec64bdb4dd58f54f03562f4cbb8f89ad5
moyongkexing <moyongkexing@gmail.com>
2021-03-25 22:43:49 +0900

    💄 change transition-duration

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -16,7 +16,7 @@ const HomeContent: VFC<Props> = (props) => {
     // hoverすると上に移動
     <Link {...linkProps}>
       <a className="w-44 h-52 xl:h-44 bg-appBg m-5 border-2 border-gray-300 group overflow-hidden text-center">
-        <div className="flex justify-center items-center my-4 xl:my-0 xl:h-full xl:transition-all xl:duration-500 xl:ease-in-out transform group-hover:xl:-mt-44">
+        <div className="flex justify-center items-center my-4 xl:my-0 xl:h-full xl:transition-all xl:duration-700 xl:ease-in-out transform group-hover:xl:-mt-44">
           <Image className="" {...imgProps} alt="コンテンツの画像" />
         </div>
         <div className="xl:h-full flex justify-center items-center ">

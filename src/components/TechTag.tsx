7c29d4b9d48dfc7ae716652823f012a42f6b3d09
moyongkexing <moyongkexing@gmail.com>
2021-04-02 23:25:56 +0900

    🩹 simple fix

--- src/components/TechTag.tsx
+++ src/components/TechTag.tsx
@@ -1,15 +1,13 @@
 import { VFC } from "react";
+import TechTypes from "src/enums/TechTypes";
 
-interface Props {
+interface PROPS {
   name: string;
 }
 
-const TechTag: VFC<Props> = (props) => {
-
+const TechTag: VFC<PROPS> = (props) => {
   const { name } = props;
-
   return <div className="text-xs p-1 mx-1 rounded-sm shadow-inner bg-tag text-white sm:text-sm font-medium">{name}</div>;
 }
 
 export default TechTag;
-

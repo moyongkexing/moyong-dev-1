13df1888b7f0ee337ab77dbfb382cef01b1609ee
moyongkexing <moyongkexing@gmail.com>
2021-07-27 17:48:21 +0900

    📝 update sentences

--- src/variables/portfolioItems.ts
+++ src/variables/portfolioItems.ts
@@ -5,10 +5,10 @@ const PORTFOLIO_ITEMS = [
     href: "https://blackjack-game-oop.vercel.app/",
     src: "/blackjack.png",
     name: "Black Jack Game",
-    created_at: "2021 / 6",
+    created_at: "2021/6",
     tags: [TechTypes.html, TechTypes.css, TechTypes.typescript, TechTypes.tailwind, TechTypes.vercel],
     text: [
-      "オンライン学習プラットフォーム RecursionCS での課題作品で、その中で最も苦労したプロジェクトです。ユーザーはまず賭け金を選択して、それ以降は手持ちのカードの合計値に応じてアクションを選択します。ゲーム仕様は教材内で提示されたものを参考にしていますが、実装は完全自力です。",
+      "オンライン学習プラットフォーム RecursionCS での課題作品で、その中で最も苦労したプロジェクトです。ユーザーはまず賭け金を調節して、それ以降は手持ちのカードの合計値に応じてアクションを選択します。ゲーム仕様は教材内で提示されたものを参考にしていますが、実装は完全自力です。",
       "100%オブジェクト指向で書いていて、いかに直感的に読めるように書くか？MVCをどう分離するか？が一番悩みました。Modelはすっきり書けたという自信がありますが、ViewとControllerが膨大なのが課題です(責務の区別が難しい)。また継承や抽象クラス、インターフェース、enum型、private修飾子などを使って堅牢なつくりを目指しました。ボット(AI)の判断の精度や、デザインにもこだわりました。(結局HTML/CSSが一番難しい...)",
     ],
   },
@@ -45,8 +45,8 @@ const PORTFOLIO_ITEMS = [
     tags: [TechTypes.next, TechTypes.typescript, TechTypes.tailwind, TechTypes.vercel],
     text: [
       "2つ目の自己紹介用ホームページです。",
-      "Next.jsでのSSG、ISRに手を出してみたくて作り始めましたがRecursionCSというサービスに出会い、ただのポートフォリオ置き場になりました(気が向いた時に開発したいと思います)。",
-      "現時点のこだわりポイントはこのページの技術タグをenumsを使っていることです。データをmapメソッドで展開することでJSXをすっきりと(?)書くことにもこだわっています。",
+      "Next.jsでのSSG、ISRに手を出してみたくて作り始めましたがRecursionCSというサービスに出会い、ただのポートフォリオ置き場になりました。",
+      "現時点のこだわりポイントはこのページの技術タグにenumsを使っていることで、Qiitaにも投稿しました。データをmapメソッドで展開することでJSXをすっきりと(?)書くことにもこだわっています。",
     ],
   },
   {
@@ -56,10 +56,10 @@ const PORTFOLIO_ITEMS = [
     created_at: "2021/3",
     tags: [TechTypes.react, TechTypes.toolkit, TechTypes.typescript, TechTypes.firebase, TechTypes.tailwind],
     text: [
-      "日々の筋トレを記録できるWebアプリです。",
-      "主な機能としては、ログイン機能、投稿機能、コメント機能、GitHubカレンダー機能です。",
+      "Udemyのハンズオンアプリを改造してつくったSPAです。日々の筋トレ記録をシェアできます。",
+      "ログイン機能、投稿機能はミヨウミマネで実装し、自力でコメント機能とGitHubカレンダー機能を追加しました。",
       "UIはTailwind CSSとMaterial UIを使用しています。",
-      "Reduxによるステート管理やHooks、Firebaseの使い方を学びました。",
+      "Reduxによるステート管理やHooksの記法、FirebaseのAuthentication、FireStoreの使い方を学びました。",
     ],
   },
   {
@@ -83,7 +83,7 @@ const PORTFOLIO_ITEMS = [
     text: [
       "勉強はじめたての頃に、スクール講師の方の力とフレームワークの魔法をひたすら頼ってなんとか形にしたwebアプリです。",
       "機能としてはTwitterのようなつぶやき投稿機能、いいね、コメント、フォローなどが行えます。",
-      "HTML/CSSから始めて、GitやMVC、RDB、セッション、ソフトウェアテストなど、web開発の基本的な概念に一通り触れることができました。AWSデプロイとDocker運用にも挑戦しましたが挫折してしまいました。ネットの指南記事や書籍を読み漁っていましたが、今振り返れば公式ドキュメントを読み解けるだけの基礎的な知識がまず必要だったと思います。webを支える技術がどれほど広範にわたるものかを痛感しました。",
+      "HTML/CSSから始めて、GitやMVC、RDB、セッション、ソフトウェアテストなど、web開発の基本的な概念に一通り触れることができました。AWSデプロイとDocker運用にも挑戦しましたが挫折してしまいました。当時はネットの指南記事や書籍を読み漁っていましたが、今振り返れば公式ドキュメントを読み解けるだけの基礎的な知識がまず必要でした。webを支える技術がどれほど広範にわたるものか痛感しました。",
     ],
   },
 ];
